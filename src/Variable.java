70231148b7e7782cdb884cf3581df5318e96edea
编程 <1660873558@qq.com>
2021-07-07 10:32:06 +0800

    浮点整数字符类型

--- src/Variable.java
+++ src/Variable.java
@@ -20,8 +20,6 @@ public class Variable {
         System.out.println("1 * 7 = " + 1*7);
         System.out.println("1 * 8 = " + 1*8);
         System.out.println("1 * 9 = " + 1*9);
-
-
         System.out.println("2 * 2 = " + 2*2);
         System.out.println("2 * 3 = " + 2*3);
         System.out.println("2 * 4 = " + 2*4);
@@ -30,8 +28,6 @@ public class Variable {
         System.out.println("2 * 7 = " + 2*7);
         System.out.println("2 * 8 = " + 2*8);
         System.out.println("2 * 9 = " + 2*9);
-
-
         System.out.println("3 * 3 = " + 3*3);
         System.out.println("3 * 4 = " + 3*4);
         System.out.println("3 * 5 = " + 3*5);
@@ -39,38 +35,27 @@ public class Variable {
         System.out.println("3 * 7 = " + 3*7);
         System.out.println("3 * 8 = " + 3*8);
         System.out.println("3 * 9 = " + 3*9);
-
-
         System.out.println("4 * 4 = " + 4*4);
         System.out.println("4 * 5 = " + 4*5);
         System.out.println("4 * 6 = " + 4*6);
         System.out.println("4 * 7 = " + 4*7);
         System.out.println("4 * 8 = " + 4*8);
         System.out.println("4 * 9 = " + 4*9);
-
-
         System.out.println("5 * 5 = " + 5*5);
         System.out.println("5 * 6 = " + 5*6);
         System.out.println("5 * 7 = " + 5*7);
         System.out.println("5 * 8 = " + 5*8);
         System.out.println("5 * 9 = " + 5*9);
-
-
         System.out.println("6 * 6 = " + 6*6);
         System.out.println("6 * 7 = " + 6*7);
         System.out.println("6 * 8 = " + 6*8);
         System.out.println("6 * 9 = " + 6*9);
-
-
         System.out.println("7 * 7 = " + 7*7);
         System.out.println("7 * 8 = " + 7*8);
         System.out.println("7 * 9 = " + 7*9);
-
         System.out.println("8 * 8 = " + 8*8);
         System.out.println("8 * 9 = " + 8*9);
-
         System.out.println("9 * 9 = " + 9*9);
-
 
     }
 }
---
+++ src/Variable2.java
@@ -0,0 +1,21 @@
+public class Variable2 {
+    public static void main(String args[]) {
+
+        byte b1 =2;
+        int i1 = 130;
+
+        float f = b1 + i1;
+        System.out.println(f);
+
+        short s1 = 123;
+        double d1 = s1;
+        System.out.println(d1);
+
+        char c1 = 'a';
+        int i3 = 10;
+        int i4 = c1 + i3;
+        System.out.println(i4);
+
+
+    }
+}
