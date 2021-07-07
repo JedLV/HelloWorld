b296b916e33c3ca8954591d7e0198709e1b9620e
编程 <1660873558@qq.com>
2021-07-07 14:41:03 +0800

    Sring练习题二

---
+++ src/StringPractice2.java
@@ -0,0 +1,14 @@
+public class StringPractice2 {
+    public static void main (String[] args) {
+
+        char c = 'a';//a=97 A=65
+        int num = 10;
+
+        System.out.println("* *");
+        System.out.println('*' + '\t' + '*');
+        System.out.println('*' + "\t" + '*');
+        System.out.println('*' +'\t' + "*");
+        System.out.println('*' +('\t' + "*"));
+
+    }
+}
