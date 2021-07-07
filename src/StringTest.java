e99d890c4d912260a72bd007152edf71d488a8e7
编程 <1660873558@qq.com>
2021-07-07 14:09:37 +0800

    Sring类型变量的使用

---
+++ src/StringTest.java
@@ -0,0 +1,22 @@
+public class StringTest {
+    public  static void main (String[] args){
+
+        String s1 = "Hello World!";
+        System.out.println(s1);
+
+        String s2 = "a";
+        String s3 = "";
+
+        //char c ="";//编译不通过
+
+        //***********
+        int number = 1811200039;
+        String numberStr = "学号：";
+        String inf0 = numberStr + number;//+: 链接运算
+        boolean b1 = true;
+        //String info1 = info + b1;// +:连接运算
+        //System.out.println(info1);
+
+
+    }
+}
