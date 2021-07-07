427f04b4a21ac1dd6ee8c67bd05e962233e9c413
编程 <1660873558@qq.com>
2021-07-07 09:32:54 +0800

    九九乘法表

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
-    public  static void main (String args[]){
-        System.out.println ("Hello World!");
+    public static void main(String args[]) {
+        System.out.println("Hello World!");//
+        //System.out.println ("Hello World!");
+        System.out.println("Hello World!");
+        //单行注释
+        /*
+        多行注释
+        */
     }
 }
---
+++ src/Variable.java
@@ -0,0 +1,76 @@
+public class Variable {
+    public static void main(String args[]){
+
+        byte b1 = 23;
+        int i1 =129;
+        //编译不通过
+        //int i2 = b1 + i1;
+        int i2 = b1 + i1;
+        long l1 = b1 + i1;
+        System.out.println(i2);
+        System.out.println("b1 + i1 = "+i2);
+
+
+        System.out.println("1 * 1 = " + 1*1);
+        System.out.println("1 * 2 = " + 1*2);
+        System.out.println("1 * 3 = " + 1*3);
+        System.out.println("1 * 4 = " + 1*4);
+        System.out.println("1 * 5 = " + 1*5);
+        System.out.println("1 * 6 = " + 1*6);
+        System.out.println("1 * 7 = " + 1*7);
+        System.out.println("1 * 8 = " + 1*8);
+        System.out.println("1 * 9 = " + 1*9);
+
+
+        System.out.println("2 * 2 = " + 2*2);
+        System.out.println("2 * 3 = " + 2*3);
+        System.out.println("2 * 4 = " + 2*4);
+        System.out.println("2 * 5 = " + 2*5);
+        System.out.println("2 * 6 = " + 2*6);
+        System.out.println("2 * 7 = " + 2*7);
+        System.out.println("2 * 8 = " + 2*8);
+        System.out.println("2 * 9 = " + 2*9);
+
+
+        System.out.println("3 * 3 = " + 3*3);
+        System.out.println("3 * 4 = " + 3*4);
+        System.out.println("3 * 5 = " + 3*5);
+        System.out.println("3 * 6 = " + 3*6);
+        System.out.println("3 * 7 = " + 3*7);
+        System.out.println("3 * 8 = " + 3*8);
+        System.out.println("3 * 9 = " + 3*9);
+
+
+        System.out.println("4 * 4 = " + 4*4);
+        System.out.println("4 * 5 = " + 4*5);
+        System.out.println("4 * 6 = " + 4*6);
+        System.out.println("4 * 7 = " + 4*7);
+        System.out.println("4 * 8 = " + 4*8);
+        System.out.println("4 * 9 = " + 4*9);
+
+
+        System.out.println("5 * 5 = " + 5*5);
+        System.out.println("5 * 6 = " + 5*6);
+        System.out.println("5 * 7 = " + 5*7);
+        System.out.println("5 * 8 = " + 5*8);
+        System.out.println("5 * 9 = " + 5*9);
+
+
+        System.out.println("6 * 6 = " + 6*6);
+        System.out.println("6 * 7 = " + 6*7);
+        System.out.println("6 * 8 = " + 6*8);
+        System.out.println("6 * 9 = " + 6*9);
+
+
+        System.out.println("7 * 7 = " + 7*7);
+        System.out.println("7 * 8 = " + 7*8);
+        System.out.println("7 * 9 = " + 7*9);
+
+        System.out.println("8 * 8 = " + 8*8);
+        System.out.println("8 * 9 = " + 8*9);
+
+        System.out.println("9 * 9 = " + 9*9);
+
+
+    }
+}
